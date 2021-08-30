3779b2e62a85e186a5133094b114612ff3e20fd8
shubham asthana <shuastha@cisco.com>
2021-08-30 14:50:57 +0530

    added form editability

--- src/employee-form.js
+++ src/employee-form.js
@@ -1,9 +1,28 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { Button, Modal } from '@material-ui/core';
 import './employee-form.css';
+import {employee} from './employee-data';
 
 
 export const EmployeeForm = ({data}) => {
+    const [formState, setFormState] = useState(data);
+    function saveHandler() {
+        
+    }
+    function updateNameHandler(e) {
+        setFormState({...formState, name: e.target.value})
+    }
+    function updatedesignationHandler(e) {
+        setFormState({...formState, designation: e.target.value})
+    }
+
+    function updatePhoneHandler(e) {
+        setFormState({...formState, phone: e.target.value})
+    }
+
+    function updateLocationHandler(e) {
+        setFormState({...formState, location: e.target.value})
+    }
    
     return <React.Fragment>
         {
@@ -12,14 +31,14 @@ export const EmployeeForm = ({data}) => {
             <div className="form-element">
                 <form>
                     <label>Name: </label><br/>
-                    <input value={data?.name}></input><br/>
+                    <input value={formState?.name} onChange={updateNameHandler}></input><br/>
                     <label>Designation: </label><br/>
-                    <input value={data?.designation}></input><br/>
+                    <input value={formState?.designation} onChange={updatedesignationHandler}></input><br/>
                     <label>Phone: </label><br/>
-                    <input value={data?.phone}></input><br/>
+                    <input value={formState?.phone} onChange={updatePhoneHandler}></input><br/>
                     <label>Location: </label><br/>
-                    <input value={data?.location}></input><br/>
-                    <button>Save</button>
+                    <input value={formState?.location} onChange={updateLocationHandler}></input><br/>
+                    <button onClick={saveHandler}>Save</button>
                 </form>
                 
             </div>
